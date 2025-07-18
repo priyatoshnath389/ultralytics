92623265173d8411372cb640e3db9fd623de4baf
priyatosh priyadarshi nath <123bm0756@nitrkl.ac.in>
2025-07-18 11:03:43 +0630

    Update __init__.py
    Signed-off-by: priyatosh priyadarshi nath <123bm0756@nitrkl.ac.in>

--- ultralytics/models/__init__.py
+++ ultralytics/models/__init__.py
@@ -5,5 +5,7 @@ from .nas import NAS
 from .rtdetr import RTDETR
 from .sam import SAM
 from .yolo import YOLO, YOLOE, YOLOWorld
+from ultralytics.nn.modules.cbam import CBAM
+
 
 __all__ = "YOLO", "RTDETR", "SAM", "FastSAM", "NAS", "YOLOWorld", "YOLOE"  # allow simpler import
